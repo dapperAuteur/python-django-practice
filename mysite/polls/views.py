a4a7873437f6361c5210dcf09458af30ae8604f1
Anthony McDonald <a@awews.com>
2018-08-05 14:08:46 -0400

    fix error by changing 'def IndexView' to 'class IndexView'

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,18 +7,18 @@ from django.views import generic
 
 from .models import Choice, Question
 
-def IndexView(generic.ListView):
+class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
-def DetailView(generic.DetailView):
+class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
-def ResultsView(generic.DetailView):
+class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
