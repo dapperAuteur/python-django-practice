5edc7bec28a68dda9e308c4da51a877a8e6b781f
Anthony McDonald <a@awews.com>
2018-08-05 19:13:44 -0400

    refactor admin to include Choice inline and allow adding Choice object when adding Question object

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,14 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,              {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes':
+        ['collapse']}),
     ]
-
-class ChoiceAdmin(admin.ModelAdmin):
-    fields = ['choice_text', 'question']
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice, ChoiceAdmin)
