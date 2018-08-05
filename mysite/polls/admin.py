8386b4a0d087bec41a59c69b7efdf2224452147c
Anthony McDonald <a@awews.com>
2018-08-05 19:08:06 -0400

    add choices to admin panel/menu

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question
+from .models import Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldssets = [
+        (None,              {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+class ChoiceAdmin(admin.ModelAdmin):
+    fields = ['choice_text', 'question']
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
