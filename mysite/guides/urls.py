c1525ae68e68088a249da65a8cdf22180a9a67e4
dapperauteur <a@awews.com>
2018-08-06 14:42:41 -0400

    add urls and views for funder and contributor page

--- mysite/guides/urls.py
+++ mysite/guides/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # Contributors Page ex: /contribute/
+    path('contributor', views.detail, name='contributor'),
+    # Funders Page ex: /fund/
+    path('funder', views.detail, name='funder'),
 ]
--- mysite/guides/views.py
+++ mysite/guides/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Index Page that links to Funder and Contributor pages.")
 
+def contributor(request):
+    return HttpResponse("This is the Contributor Page")
+
+def funder(request):
+    return HttpResponse("This is the Funder Page")
+
 # Create your views here.
